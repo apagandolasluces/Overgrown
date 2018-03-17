52713a1279bd7f459dfd1f1abaf5102bcaa0043a
apagandolasluces <nicklacroix.17@gmail.com>
2018-03-17 15:39:31 -0700

    added tankbeetle and worm scripts and sprite

---
+++ tankbeetle.gd
@@ -0,0 +1,41 @@
+extends Node2D
+
+signal landed
+
+const GRAVITY = 200
+
+onready var hp = 50
+onready var spd = 75
+onready var grounded = false
+
+var screen_size
+
+func _ready():
+ screen_size = get_viewport_rect().size
+ set_process(true)
+ pass
+
+func _process(delta):
+ if(!grounded):
+  position.y += GRAVITY * delta
+  #ground is at y = 504
+ if(position.x < 40):
+  position.x = 40
+ if(position.x > screen_size.x - 40):
+  position.x = screen_size.x - 40
+
+
+
+func _on_Area2D_area_entered(area):
+ if(area.is_in_group("Platform")):
+  grounded = true
+  emit_signal("landed")
+
+
+func _on_Area2D_area_exited(area):
+ if(area.is_in_group("Platform")):
+  grounded = false
+
+
+	
+
