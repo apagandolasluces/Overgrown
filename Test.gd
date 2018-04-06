07ed5b37e3bbc8f7671e461f19e86b080e4ef94b
apagandolasluces <nicklacroix.17@gmail.com>
2018-04-06 13:52:48 -0700

    added multiple instances of enemy critters

--- Test.gd
+++ Test.gd
@@ -1,10 +1,28 @@
 extends Node
 
+onready var tankbeetle = load("res://tankbeetle.tscn")
+onready var worm = load("res://worm.tscn")
+onready var ant = load("res://ant.tscn")
+
 
 func _ready():
-	get_tree().paused = true
+    get_tree().paused = true
+    var worm_one = worm.instance()
+    worm_one.position = Vector2(993, 517)
+    worm_one.scale = Vector2(2,2)
+    add_child(worm_one)
+	
+    var worm_two = worm.instance()
+    worm_two.position = Vector2(500, 517)
+    worm_two.scale = Vector2(2,2)
+    add_child(worm_two)
 
-
+    var ant_one = ant.instance()
+    ant_one.position = Vector2(700, 501)
+    ant_one.scale = Vector2(2,2)
+    add_child(ant_one)
+	
+	
 
 func _on_shrub_landed():
 	pass
